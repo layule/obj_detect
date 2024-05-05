0b09cb90ab6967dde1c64b93fcb3bd595771dcb4
layule <dbf6655@gmail.com>
2024-05-05 22:43:24 +0900

    fix: login error 수정

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask import jsonify
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 app.secret_key = "i"
 
 ID = "hello"
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom"; // 리액트 라우터의 useHistory 훅을 불러옵니다.
 import "../style/reset.css";
 import style from "../style/Login.module.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false); // 로딩 상태를 관리합니다.
+  const [error, setError] = useState(""); // 에러 메시지를 관리합니다.
+  const history = useHistory(); // useHistory 훅을 사용하여 페이지 이동을 관리합니다.
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true); // 폼 제출 시 로딩 상태를 활성화합니다.
 
     try {
       const response = await axios.post("http://127.0.0.1:5000/login", {
@@ -16,10 +21,12 @@ const Login = () => {
         loginPW: password,
       });
 
-      window.location.href = "/";
+      setLoading(false);
+      history.push("/");
     } catch (error) {
+      setLoading(false);
       console.error("로그인 실패:", error);
-      alert("로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.");
+      setError("아이디 또는 비밀번호가 잘못되었습니다.");
     }
   };
 
@@ -38,7 +45,14 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        {loading ? (
+          <button type="button" disabled>
+            로딩 중...
+          </button>
+        ) : (
+          <button type="submit">Login</button>
+        )}
+        {error && <p className={style.error}>{error}</p>}{" "}
       </form>
     </div>
   );
