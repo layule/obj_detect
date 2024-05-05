925942f8228c6749ebb9696659e686c84d354404
layule <dbf6655@gmail.com>
2024-05-05 17:34:27 +0900

    feat: flask react fetch

---
+++ flask_app/app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return jsonify({"message": "Hello, Flask!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./style/reset.css";
 import Header from "./components/Header";
@@ -5,10 +6,21 @@ import Footer from "./components/Footer";
 import MySwiper from "./components/MySwiper";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
     <div className="page-container">
       <Header />
       <MySwiper />
+      <div>
+        <p>{message}</p>
+      </div>
       <Footer />
     </div>
   );
